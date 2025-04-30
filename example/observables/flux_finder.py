07f90477bdc0781eab82ab4596bd227a4747d467
christopherlovell <chrisl1991@hotmail.co.uk>
2025-04-30 10:27:22 +0200

    obs methods

---
+++ example/observables/flux_finder.py
@@ -0,0 +1,119 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+from astropy.cosmology import Planck18 as cosmo
+from multiprocessing import Pool
+from unyt import Hz, Msun, Myr, erg, nJy, s, angstrom, Gyr
+
+from synthesizer import galaxy
+from synthesizer.instruments import FilterCollection
+from synthesizer.grid import Grid
+from synthesizer.parametric import SFH, Stars, ZDist
+from synthesizer.emission_models.attenuation import Madau96
+from synthesizer.emission_models import IncidentEmission
+
+
+nprocs = 2
+
+with h5py.File('../data/evs_all.h5','r') as hf:
+    log10m = hf['log10m'][:]
+    f = hf['f'][:]
+    F = hf['F'][:]
+    N = hf['N'][:]
+    redshifts = hf['z'][:]
+
+
+grid_name = "test_grid"
+grid_dir = "../../../synthesizer/tests/test_grid/"
+grid = Grid(
+    grid_name,
+    grid_dir=grid_dir,
+    new_lam=np.logspace(2.3, 5, 500) * angstrom
+)
+
+model = IncidentEmission(grid=grid)
+
+
+
+filter_codes = [
+    f"JWST/{filt}" for filt in 
+    ['NIRCam.F277W', 'NIRCam.F115W', 'NIRCam.F444W', 'MIRI.F770W']
+]
+fc = FilterCollection(filter_codes, new_lam=grid.lam)
+
+flux = np.zeros((len(log10m), len(redshifts)))
+
+def create_galaxy(z, m, grid, tau): 
+    sfh = SFH.Exponential(tau, cosmo.age(z) * Gyr)
+    metal_dist = ZDist.Normal(mean=0.01, sigma=0.005)
+
+    exp_stars = Stars(
+        grid.log10age,
+        grid.metallicity,
+        sf_hist=sfh,
+        metal_dist=metal_dist,
+        initial_mass=m * Msun,
+    )
+    
+    return galaxy(stars=exp_stars, redshift=z)
+
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 5))
+
+for i, tau in enumerate([-20, -50, -100, -500, -1000]):
+    gal = create_galaxy(1, 10, grid, tau * Myr)
+    # ax.step(10**gal.stars.log10ages / 1e9, gal.stars.sf_hist, where="mid", label=f'{tau} Myr')
+    ax.step(gal.stars.log10ages, gal.stars.sf_hist, where="mid", label=f'{tau} Myr')
+    # gal.stars.plot_sfh(ax=ax)
+
+# ax.set_xlim(0, 10)
+ax.set_xlim(6, 11)
+# ax.semilogy()
+ax.legend()
+ax.set_xlabel(r"${\rm log_{10}} \; \mathrm{age} \,/\, \mathrm{Gyr}$")
+ax.set_ylabel(r"SFH")
+plt.show()
+
+
+def get_photo(z, fc, log10m, grid, tau=20 * Myr):
+    gal = create_galaxy(z, 10**log10m[0], grid, tau)
+    sed = gal.stars.get_spectra(model)
+    sed.get_fnu(cosmo, z, igm=Madau96)
+    return sed.get_photo_fnu(fc)
+
+
+# with Pool(processes=nprocs) as pool:
+#     args = [(z, fc, log10m, grid, sfh, metal_dist) 
+#     for j, z in enumerate(redshifts)]
+#     output = pool.starmap(get_photo, args)
+
+output = [get_photo(z, fc, log10m, grid) for z in redshifts]
+
+mass_ratios = 10**log10m / 10**log10m[0]
+
+# Save output
+for tau in [-20, -50, -100, -500, -1000]:
+
+    output = [get_photo(z, fc, log10m, grid, tau=tau * Myr) for z in redshifts]
+
+    for code in fc.filter_codes: 
+        flux = np.zeros((len(log10m), len(redshifts)))
+        for i, mass_ratio in enumerate(mass_ratios):
+            for j, z in enumerate(redshifts):
+                flux[i,j] = output[j][code].value * mass_ratio
+    
+        np.savetxt(f'data/flux_grid_{code.split('/')[1]}_tau_m{-1 * tau}.txt', flux)
+
+# Plot
+fig, ax = plt.subplots(1, 1, figsize=(6, 5))
+
+for m, _flux in zip(log10m, flux[::20,:]):
+    ax.plot(redshifts, _flux)
+
+ax.set_ylim(-1, 4)
+ax.set_xlabel("Redshift")
+ax.set_ylabel("Flux [nJy]")
+# ax.legend()
+plt.show()
+# plt.savefig("plots/flux_vs_redshift_fixed_mass_F277W.png", dpi=200)
+# plt.close()
---
+++ example/observables/hypersurface_sfh_variations.py
@@ -0,0 +1,125 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+from astropy.cosmology import Planck15
+import astropy.units as u
+from scipy import integrate
+from evstats import evs
+from evstats.stats import compute_conf_ints, eddington_bias
+from evstats.stellar import apply_fs_distribution
+
+
+with h5py.File('../data/evs_all.h5','r') as hf:
+    log10m = hf['log10m'][:]
+    f = hf['f'][:]
+    F = hf['F'][:]
+    N = hf['N'][:]
+    z = hf['z'][:]
+
+
+# Load flux grid data
+band = 'NIRCam.F444W'
+
+
+_obs_str = f'Casey24_{band}'
+
+whole_sky = (41252.96 * u.deg**2).to(u.arcmin**2)
+survey_area = 0.28 * u.degree**2
+fsky = float(survey_area / whole_sky)
+phi_max = evs._apply_fsky(N, f, F, fsky)
+f_b = 0.156
+
+CI_mhalo = compute_conf_ints(phi_max, log10m)
+CI_baryon = np.log10(10**CI_mhalo * f_b) 
+
+
+# mstar_pdf = np.vstack([
+#     apply_fs_distribution(
+#         _phi_max,
+#         log10m,
+#         _N=int(1e5),
+#         method='lognormal'
+#     ) for _phi_max in phi_max
+# ])
+
+
+# Confidence interval
+redshift_idx = np.arange(len(z))
+# CI_flux = np.log10(np.vstack([compute_conf_ints(mstar_pdf[i], flux_grid[:, i]) for i in redshift_idx]))
+
+low_z_colors = ['steelblue','lightskyblue','powderblue'] # ['brown','lightcoral','mistyrose']
+colors = low_z_colors
+
+fig, ax = plt.subplots(1, 1, figsize=(5,5))
+
+
+for tau in [20, 
+flux_grid = np.loadtxt(f"data/flux_grid_{band}.txt")
+CI_flux = np.log10(np.vstack([compute_conf_ints(phi_max[i], flux_grid[:, i]) for i in redshift_idx]))
+
+# ax.fill_between(z, CI_flux[:,0], CI_flux[:,6], alpha=1, color=colors[0])
+ax.fill_between(z, CI_flux[:,1], CI_flux[:,5], alpha=1, color=colors[1])
+ax.fill_between(z, CI_flux[:,2], CI_flux[:,4], alpha=1, color=colors[2])
+ax.plot(z, CI_flux[:,3], linestyle='dotted', c='black')
+# ax.plot(z, CI_baryon[:,6], linestyle='dashed', color='black')
+
+
+#z of EazY
+z_obs = np.array([10.47, 12.47, 12.10, 12.16, 9.92, 10.49, 11.19, 11.73, 11.88, 12.6, 12.7, 16.1])
+zerr = np.array([[0.42, 0.27, 0.26, 0.23, 0.30, 0.28, 0.19, 0.23, 0.54, 0.1, 0.1, 1.6],
+                [0.20, 0.02, 0.08, 0.27, 0.26, 0.22, 0.34, 0.18, 0.02, 2.5, 4.7, 2.4]])
+
+
+# F444W ("SE++ model based photometry")
+flux = np.array([246.0, 226.2, 188.2, 364.2, 260.5, 139.1, 132.9, 106.6, 106.4, 48.7, 41.3, 30.2, 86.0, 33.5, 61.5])[:12]
+flux_err = np.array([8.1, 7.1, 7.3, 8.5, 7.7, 8.6, 6.9, 6.8, 5.1, 6.3, 5.8, 4.8, 8.8, 5.7, 5.1])[:12]
+
+#F277W flux values
+# F = np.array([67.4, 56.2, 82.0, 94.2, 29.3, 46.3, 41.0, 43.5, 29.2, 44.6, 44.9, 27.8]) # Maybe need np.log F and F_err?
+# F_err = np.full((2, 12), 3.5)
+
+# M_corr, _epsilon = eddington_bias(np.log10(10**M * (1./f_b)), M_err)
+# M_corr = np.log10(10**M_corr * f_b) 
+
+# ax.errorbar(
+#     z_obs,
+#     np.log10(flux),
+#     xerr=zerr,
+#     yerr=(
+#         np.log10(flux) - np.log10(flux - flux_err),
+#         np.log10(flux + flux_err) - np.log10(flux),
+#     ),
+#     fmt='o',
+#     c='orange'
+# )
+
+# ax.errorbar(z_obs, M_corr, xerr=zerr, yerr=M_err, fmt='o', c='orange', label='Casey24')
+
+ax.set_xlim(2, 18)
+ax.set_ylim(-3,8)
+ax.set_xlabel('$z$', size=17)
+ax.set_ylabel("Flux [nJy]", size = 17)
+#ax.set_ylabel('$\mathrm{log_{10}}(M^{\star}_{\mathrm{max}} \,/\, M_{\odot})$', size=15)
+ax.text(0.05, 0.04, r'$A = 0.28 \; \mathrm{arcmin}^2$', size=12, color='black', alpha=0.8, transform = ax.transAxes)
+
+leg = ax.legend(frameon=False, bbox_to_anchor=(0.44,0.19), fontsize=12, handletextpad=0.2) 
+plt.gca().add_artist(leg) # Add the legend manually to the current Axes.
+
+line1 = plt.Line2D((0,1),(0,0), color=colors[0], linewidth=5)
+line2 = plt.Line2D((0,1),(0,0), color=colors[1], linewidth=5)
+line3 = plt.Line2D((0,1),(0,0), color=colors[2], linewidth=5)
+line4 = plt.Line2D((0,1),(0,0), color='black', linestyle='dotted', linewidth=2)
+line5 = plt.Line2D((0,1),(0,0), color='black', linestyle='dashed', linewidth=2)
+line_dummy = plt.Line2D((0,1),(0,0), color='white')
+leg = ax.legend(handles=[line4,line5,line_dummy,line3,line2,line1], 
+           labels=[r'$\mathrm{med}(M^{\star}_{\mathrm{max}})$', r'$f_{\star} = 1$; $+3\sigma$','',
+               r'$1\sigma$', r'$2\sigma$', r'$3\sigma$'],
+                frameon=False, loc='upper right', fontsize=12, ncol=2)
+
+vp = leg._legend_box._children[-1]._children[0] 
+for c in vp._children: c._children.reverse() 
+vp.align="right" 
+
+# plt.show()
+plt.savefig(f'plots/evs_{_obs_str}.png', bbox_inches='tight', dpi=200)
