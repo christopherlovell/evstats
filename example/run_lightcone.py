cc5d6e2446d94d72530c04ef6c8344feaf5b34d1
christopherlovell <chrisl1991@hotmail.co.uk>
2025-05-03 19:19:18 +0200

    parallelise grid creation

--- example/run_lightcone.py
+++ example/run_lightcone.py
@@ -22,9 +22,11 @@ def zlim_pdf(zlims, mass_function=hmf.MassFunction(hmf_model='Behroozi'), mmin=4
     zmin = zlims[0]
     zmax = zlims[1]    
 
+    # print(zmin, zmax)
     N, f, F, ln10m_range = evs._evs_bin(mf=mass_function, zmin=zmin, zmax=zmax, 
                                         mmin=mmin, mmax=mmax, dm=dm, dz=dz)
-    
+
+    print(zmin, N, f[0], F[0])    
     return N, f, F, ln10m_range
 
 
@@ -34,39 +36,54 @@ if __name__ == '__main__':
     # _out_str = 'evs_pdf'
     
     if os.path.exists('data/%s'%_out_str):
-        if sb.yesno('File exists. Are you sure you wish to overwrite the existing file?'):
-            os.remove('data/%s'%_out_str)
-        else:
-            print("Exiting...")
-            sys.exit()
+        # if sb.yesno('File exists. Are you sure you wish to overwrite the existing file?'):
+        os.remove('data/%s'%_out_str)
+        # else:
+        #     print("Exiting...")
+        #     sys.exit()
 
     
-    zlowlim = np.arange(0, 19.8, 0.2)
-    zupplim = np.arange(0.2, 20, 0.2)
+    # zlowlim = np.arange(0, 14.9, 0.1)
+    # zupplim = np.arange(0.1, 15, 0.1)
+    
+    zlowlim = np.arange(0, 44.9, 0.5)
+    zupplim = np.arange(0.1, 45, 0.5)
     zcentre = zlowlim + np.diff(zlowlim)[0]/2
     
-    dz = 0.01
-    mmin = 5; mmax = 17; dm=0.01
+    dz = 0.05  # 0.01
+    mmin = 4; mmax = 14; dm=0.03  # 0.01
     
-    mass_function = hmf.MassFunction(hmf_model='Behroozi')
+    mass_function = hmf.MassFunction()  # hmf_model='Behroozi')
     mass_function.cosmo_model = Planck15
     
-    NUM_PROCS = multiprocessing.cpu_count()
-    # NUM_PROCS = 8
+    # NUM_PROCS = multiprocessing.cpu_count()
+    NUM_PROCS = 8
     print(NUM_PROCS)
     sys.stdout.flush()
 
-    pool = multiprocessing.Pool(processes=NUM_PROCS)
-    results = pool.map(partial(zlim_pdf, mass_function=mass_function, 
-                               mmin=mmin, mmax=mmax, dm=dm, dz=dz), \
-                       zip(zlowlim, zupplim))
+    # pool = multiprocessing.Pool(processes=NUM_PROCS)
+    # results = pool.map(partial(zlim_pdf, mass_function=mass_function, 
+    #                            mmin=mmin, mmax=mmax, dm=dm, dz=dz), \
+    #                    zip(zlowlim, zupplim))
+
+    results = [
+        zlim_pdf(
+            (_zlowlim, _zupplim),
+            mass_function=mass_function,
+            mmin=mmin,
+            mmax=mmax,
+            dm=dm,
+            dz=dz
+        ) for _zlowlim, _zupplim in zip(zlowlim, zupplim)
+    ]
+        
     
     N = np.vstack([dat[0] for dat in results])
     f = np.vstack([dat[1] for dat in results])
     F = np.vstack([dat[2] for dat in results])
     log10m = results[0][3]
     
-    with h5py.File(_out_str, 'w') as hf:
+    with h5py.File(f'data/{_out_str}', 'w') as hf:
         hf.create_dataset('log10m', data=log10m)
         hf.create_dataset('z', data=zcentre)
         hf.create_dataset('N', data=N)
